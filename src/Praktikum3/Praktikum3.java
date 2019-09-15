fa16292a20e3eb99a3dea2012f6eb54ec611bcad
Kresniawati <44692929+Kresniawati@users.noreply.github.com>
2019-09-15 22:07:19 +0700

    tugas1-3

---
+++ src/Praktikum3/Praktikum3.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Praktikum3;
+
+import Praktikum2.*;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Praktikum3 {
+    public static void main(String[] args) {
+        
+    }
+
+}
